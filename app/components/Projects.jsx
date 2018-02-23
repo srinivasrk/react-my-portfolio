603db74d26c647bc3398dae11565411d271882e8
srinivasrk <srinivasrk92@gmail.com>
2018-02-22 19:00:37 -0500

    updated projects page

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,34 +1,37 @@
 var React = require('react');
 var createClass = require('create-react-class');
 import Foundation from 'react-foundation';
-var {Menu, MenuItem, Column, Thumbnail, Row,isColumn} = require('react-foundation');
-
+var {Menu, MenuItem, Column, Thumbnail,Column, Row,isColumn} = require('react-foundation');
+import ProjectImage from './project_background.png';
+import WebApp from './web-app.png';
 var Projects = createClass({
   render:function(){
     return(
-      // <div className="grid-block-example">
-      //   <Row upOnSmall={1} upOnMedium={2} upOnLarge={3}>
-      //     <Column isColumn>
-      //       <Thumbnail src="//unsplash.it/300" alt=""/>
-      //     </Column>
-      //     <Column isColumn>
-      //       <Thumbnail src="//unsplash.it/300" alt=""/>
-      //     </Column>
-      //     <Column isColumn>
-      //       <Thumbnail src="//unsplash.it/300" alt=""/>
-      //     </Column>
-      //     <Column isColumn>
-      //       <Thumbnail src="//unsplash.it/300" alt=""/>
-      //     </Column>
-      //     <Column isColumn>
-      //       <Thumbnail src="//unsplash.it/300" alt=""/>
-      //     </Column>
-      //     <Column isColumn>
-      //       <Thumbnail src="//unsplash.it/300" alt=""/>
-      //     </Column>
-      //   </Row>
-      // </div>
-      <h2> Project component</h2>
+      <div className="carousel_container">
+        <img className="about_container" src ={ProjectImage} />
+        <div className="project_container">
+          <Row className="display project_item">
+          <Column small={3} large={3}>  <img className="project_icon" src = {WebApp} /> </Column>
+          <Column small={9} large={9} className="project_desc">  <p className="project_title"> PS Dashboard</p> </Column>
+          </Row>
+          <hr />
+            <Row className="display project_item">
+            <Column small={3} large={3}>  <img className="project_icon" src = {WebApp} /> </Column>
+            <Column small={9} large={9} className="project_desc">  <p className="project_title"> PS Dashboard</p> </Column>
+            </Row>
+            <hr />
+              <Row className="display project_item">
+              <Column small={3} large={3}>  <img className="project_icon" src = {WebApp} /> </Column>
+              <Column small={9} large={9} className="project_desc">  <p className="project_title"> PS Dashboard</p> </Column>
+              </Row>
+              <hr />
+                <Row className="display project_item">
+                <Column small={3} large={3}>  <img className="project_icon" src = {WebApp} /> </Column>
+                <Column small={9} large={9} className="project_desc">  <p className="project_title"> PS Dashboard</p> </Column>
+                </Row>
+                <hr />
+        </div>
+      </div>
     )
   }
 });
