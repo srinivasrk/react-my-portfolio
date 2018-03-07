995be6672c177ba3ca7091cf4496fa4c2bc7ded9
srinivasrk <srinivasrk92@gmail.com>
2018-03-07 16:32:49 -0500

    first public push

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,6 +6,7 @@ var {Row, Column} = require('react-foundation');
 import GradIcon from './grad_icon.png';
 import ExpIcon from './exp_icon.png';
 import MeIcon from './me_icon.png';
+import MyImg from './profile_crop.png';
 
 var About = createClass({
   render: function(){
@@ -13,8 +14,22 @@ var About = createClass({
         <div className="carousel_container">
           <img className="about_container" src ={profile_img} />
            <div className="overlay_education">
+            <img src={MyImg} className="profile_img" ></img>
+            <p className="my_desc">
+              “Anyone can write code that a computer can understand. Good programmers write code that humans can understand.”  ― Martin Fowler
+              <br />
+              <br />
+              </p>
+            <p className="my_desc_2">
+              I am a programmer by heart and soul. I write code to solve problems and make things easier.
+              I love to travel and meet people. I have 3 years of experience working at EMC as an Sr. Analyst &currently I am a masters of engineering student at University of Cincinnati.
+            </p>
+            <p className="my_desc_2">
+              I worked on technologies like .NET, C# windows application development. I am also experienced in
+              ASP.NET and web development. I work as an MATLAB teaching assistant for University of Cincinnati
 
-           </div> 
+            </p>
+           </div>
         </div>
     )
   }
--- app/components/Details.jsx
+++ app/components/Details.jsx
@@ -4,6 +4,9 @@ import Foundation from 'react-foundation';
 var {Menu, MenuItem, Column, Thumbnail, Row,isColumn} = require('react-foundation');
 import MoreDetails from './more_details.png';
 import LinkedInLogo from './linkedin_logo.png';
+import EmailLogo from './tiny_email_logo.png';
+import BitbucketLogo from './bitbucket_logo.png';
+import CVLogo from './cv_logo.png';
 
 var Details = createClass({
   render:function(){
@@ -12,10 +15,17 @@ var Details = createClass({
         <img className="about_container" src ={MoreDetails} />
         <div className="project_container">
           <p className="details_title"> Lets get in touch </p>
-          <p className="details_SmallPara"> I can help you build your next idea!</p>
-          <p className="details_SmallPara"> You can reach me at srinivasrk92@gmail.com </p>
-          <p> Bitbucket, LinkedIn, tech stack I work on </p>
-          <img src={LinkedInLogo} className="tiny_logos" />
+          <div className="logo_list">
+            <a href="https://www.linkedin.com/in/srini-cincy/" target="_blank"><img src={LinkedInLogo} className="linkedin_logo" /></a>
+            <a href="mailto:srinivasrk92@gmail.com"><img src={EmailLogo} className="email_logo" /></a>
+            <a href="https://bitbucket.org/srinivasrk" target="_blank"><img src={BitbucketLogo} className="bitbucket_logo" /></a>
+            <a href="https://1drv.ms/b/s!Al61Meua1OkRgYkgo786XFUnACl5sw" target="_blank"><img src={CVLogo} className="cv_logo" /></a>
+          </div>
+          <p className="my_desc_2">
+            I am currently looking for internships / full time positions starting summer 2018.
+            My area of expertise includes android, C# .NET, ASP.NET, HTML, CSS, JavaScript, React, PHP, Python.
+            I have also worked on machine learning libraries using R programming & have knowledge on probabilistic models and statistical modelling
+          </p>
         </div>
       </div>
     )
